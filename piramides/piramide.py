3dc7f3d53b03f15df37bd3e242ad7bb423d29470
ConradoLuiz <conradol.vargas@gmail.com>
2020-11-01 01:46:48 -0300

    mudanca na piramide

--- piramides/piramide.py
+++ piramides/piramide.py
@@ -2,53 +2,72 @@ from OpenGL.GLUT import *
 from OpenGL.GLU import *
 from OpenGL.GL import *
 
-width, height = 800.0, 600.0
+width, height = 1200.0, 800.0
 rotation_delay = 10
 screen_translate = 0.1
 
+zoom = 0.1
+
+pMouseX = None
+pMouseY = None
+isDragging = False
+
 def piramide():
-    glBegin( GL_TRIANGLES )
+    glBegin( GL_TRIANGLE_FAN )
 
     glColor3f( 1.0, 0.0, 0.0 ) 
-    glVertex3f( 0.0, 1.0, 0.0 )
+    glVertex3f(0.0, 1.0, 0.)
 
     glColor3f( 0.0, 1.0, 0.0 ) 
-    glVertex3f( -1.0, -1.0, 1.0 )
+    glVertex3f(-1., -1.0, 1.)
+    
+    glVertex3f(1., -1.0, 1.)
 
-    glColor3f( 0.0, 0.0, 1.0 ) 
-    glVertex3f( 1.0, -1.0, 1.0)
+    glVertex3f(1., -1., -1.)
+    
+    glVertex3f(-1., -1., -1.)
 
-
-    glColor3f( 1.0, 0.0, 0.0 ) 
-    glVertex3f( 0.0, 1.0, 0.0)
-
-    glColor3f( 0.0, 1.0, 0.0 ) 
-    glVertex3f( -1.0, -1.0, 1.0)
-
-    glColor3f( 0.0, 0.0, 1.0 ) 
-    glVertex3f( 0.0, -1.0, -1.0)
-
-
-    glColor3f( 1.0, 0.0, 0.0 ) 
-    glVertex3f( 0.0, 1.0, 0.0)
-
-    glColor3f( 0.0, 1.0, 0.0 ) 
-    glVertex3f( 0.0, -1.0, -1.0)
-
-    glColor3f( 0.0, 0.0, 1.0 ) 
-    glVertex3f( 1.0, -1.0, 1.0)
-
-
-    glColor3f( 1.0, 0.0, 0.0 ) 
-    glVertex3f( -1.0, -1.0, 1.0)
-
-    glColor3f( 0.0, 1.0, 0.0 ) 
-    glVertex3f( 0.0, -1.0, -1.0)
-
-    glColor3f( 0.0, 0.0, 1.0 ) 
-    glVertex3f( 1.0, -1.0, 1.0)
+    glVertex3f(-1., -1., 1.)
 
     glEnd()
+
+    glBegin(GL_QUADS)
+    
+    glColor3f( 0.0, 0.0, 1.0 ) 
+    glVertex3f(1.,-1.,1.)
+
+    glVertex3f(1.,-1.,-1.)
+
+    glVertex3f(-1.,-1.,-1.)
+
+    glVertex3f(-1.,-1.,1.)
+
+    glEnd()
+
+def mouseFunc(btn, state, mouseX, mouseY):
+    # btn -> 0 == botao esquerdo; 1 == botao rolagem; 2 == botao direito; 3 == rolagem para cima; 4 == rolagem para baixo
+    global pMouseX
+    global pMouseY
+    global isDragging
+
+    if btn == 3:
+        glTranslatef(0,0,zoom)
+    if btn == 4:
+        glTranslatef(0,0,-zoom)
+    if btn == 0 and state == 0:
+        pMouseX = mouseX
+        pMouseY = mouseY
+        isDragging = True
+    if btn == 0 and state == 1:
+        isDragging = False
+
+def mouseDrag(x,y):
+    if isDragging:
+        global pMouseX
+        global pMouseY
+        glTranslatef(5 * (x-pMouseX)/width ,5 * -(y-pMouseY)/height  ,0)
+        pMouseX = x
+        pMouseY = y
 
 
 
@@ -96,5 +115,7 @@ if __name__ == '__main__':
     glTranslatef(0.0,0.0,-8)
     glutTimerFunc(rotation_delay,timer,1)
     glutKeyboardFunc(handleKey)
+    glutMouseFunc(mouseFunc)
+    glutMotionFunc(mouseDrag)
     # glRotatef(45,1,1,1)
     glutMainLoop()
