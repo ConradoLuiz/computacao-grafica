aebd6aa494a7bd6b46f840b332dcf72ea6ab7691
ConradoLuiz <conradol.vargas@gmail.com>
2020-11-02 02:23:53 -0300

    ajustes

--- paraboloide/exemplo.py
+++ paraboloide/exemplo.py
@@ -19,34 +19,49 @@ pMouseX = None
 pMouseY = None
 isDragging = False
 
+a = 0
 
-def paraboloide(x,y):
+
+def paraboloide(x, y):
     # Paraboloide Circular
     return x**2+y**2
 
+
+v = []
+x = x0
+while x < xf:
+    y = y0
+    while y < yf:
+        z = paraboloide(x, y)
+        v += [[x, y, z]]
+        y += dy
+    x += dx
+
+
 def desenhaFuncao():
     glBegin(GL_POINTS)
-    x = x0
-    while x < xf:
-        y = y0
-        while y < yf:
-            z = paraboloide(x,y)
-            glColor3f(x,y,z)
-            glVertex3f(x,y,z)
-            y += dy
-        x += dx
+
+    i = 0
+    for vertex in v:
+        glColor3f(vertex[0], vertex[1], vertex[2])
+        glVertex3f(vertex[0], vertex[1], vertex[2])
+        i += 1
+
     glEnd()
+
 
 def desenha():
     global a
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glPushMatrix()
-    glRotatef(-a, 1,0,0)
+    glRotatef(-a, 1, 0, 0)
     desenhaFuncao()
     glPopMatrix()
     glutSwapBuffers()
     a += 1
     return
+
+
 def mouseFunc(btn, state, mouseX, mouseY):
     # btn -> 0 == botao esquerdo; 1 == botao rolagem; 2 == botao direito; 3 == rolagem para cima; 4 == rolagem para baixo
     global pMouseX
@@ -54,9 +69,9 @@ def mouseFunc(btn, state, mouseX, mouseY):
     global isDragging
 
     if btn == 3:
-        glTranslatef(0,0,zoom)
+        glTranslatef(0, 0, zoom)
     if btn == 4:
-        glTranslatef(0,0,-zoom)
+        glTranslatef(0, 0, -zoom)
     if btn == 0 and state == 0:
         pMouseX = mouseX
         pMouseY = mouseY
@@ -64,33 +79,35 @@ def mouseFunc(btn, state, mouseX, mouseY):
     if btn == 0 and state == 1:
         isDragging = False
 
-def mouseDrag(x,y):
+
+def mouseDrag(x, y):
     if isDragging:
         global pMouseX
         global pMouseY
-        glTranslatef(5 * (x-pMouseX)/width ,5 * -(y-pMouseY)/height  ,0)
+        glTranslatef(5 * (x-pMouseX)/width, 5 * -(y-pMouseY)/height, 0)
         pMouseX = x
         pMouseY = y
 
 
 def timer(i):
     glutPostRedisplay()
-    glutTimerFunc(rotation_delay,timer,1)
+    glutTimerFunc(rotation_delay, timer, 1)
+
 
 if __name__ == '__main__':
     # PROGRAMA PRINCIPAL
-    a = 0
     glutInit(sys.argv)
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH | GLUT_MULTISAMPLE)
-    glutInitWindowSize(int(width),int(height))
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA |
+                        GLUT_DEPTH | GLUT_MULTISAMPLE)
+    glutInitWindowSize(int(width), int(height))
     glutCreateWindow("Paraboloide")
     glutDisplayFunc(desenha)
     glEnable(GL_MULTISAMPLE)
     glEnable(GL_DEPTH_TEST)
-    glClearColor(0.,0.,0.,1.)
-    gluPerspective(45,width/height,0.1,100.0)
-    glTranslatef(0.0,-0.4,-6)
-    glutTimerFunc(rotation_delay,timer,1)
+    glClearColor(0., 0., 0., 1.)
+    gluPerspective(45, width/height, 0.1, 100.0)
+    glTranslatef(0.0, -0.4, -6)
+    glutTimerFunc(rotation_delay, timer, 1)
     glutMouseFunc(mouseFunc)
     glutMotionFunc(mouseDrag)
     glutMainLoop()
