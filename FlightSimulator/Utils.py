9c37b902c7863238379e8bde7cd1e9dfb872f51b
ConradoLuiz <conradol.vargas@gmail.com>
2020-12-05 16:29:48 -0300

    nd dms

--- FlightSimulator/Utils.py
+++ FlightSimulator/Utils.py
@@ -1,5 +1,15 @@
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from OpenGL.GLUT import *
 import math
 
 
 def lerp(x0, x1, amt):
     return (1 - amt) * x0 + amt * x1
+
+
+def printText(x, y, r, g, b, string):
+    glColor3f(r, g, b)
+    glRasterPos2f(x, y)
+    for char in string:
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, ord(char))
